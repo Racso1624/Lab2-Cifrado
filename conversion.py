8211ecf2450b278fe9fd8a299dacb0d84ebaaf42
Racso1624 <fer10_lb@outlook.com>
2024-02-15 22:35:02 -0600

    Se agregan incisos 1 y 2

--- conversion.py
+++ conversion.py
@@ -1,4 +1,27 @@
-def letter_conversion(letter):
-    return bin(ord(letter))[2:]
+import base64
 
+def text_to_bits(text):
+    result_bits = ""
+    for letter in text:
+        bits = bin(ord(letter))[2:].zfill(8)
+        result_bits += bits
+    return result_bits
 
+def bits_to_text(bits):
+    result_text = ""
+    for i in range(0, len(bits), 8):
+        bit_group = bits[i:i+8]
+        value = int(bit_group, 2)
+        result_text += chr(value)
+    return result_text
+
+def text_to_base64(text):
+    bits = text_to_bits(text)
+    while len(bits) % 6 != 0:
+        bits += '0'
+    result_base64 = ""
+    for i in range(0, len(bits), 6):
+        bit_group = bits[i:i+6]
+        value = int(bit_group, 2)
+        result_base64 += base64.b64encode(bytes([value])).decode()
+    return result_base64
--- main.py
+++ main.py
@@ -1,6 +1,42 @@
-from conversion import letter_conversion
+from conversion import *
 
-word = "HOLA"
+print("Parte A - Inciso 1 a 4")
 
-for i in word:
-    print(letter_conversion(i))
+print ("\nInciso 1")
+
+text1 = "Hola"
+text2 = "Cifrado"
+
+print("\nEjemplo 1:")
+print("Texto:", text1)
+print("Bits:", text_to_bits(text1))
+
+print("\nEjemplo 2:")
+print("Texto:", text2)
+print("Bits:", text_to_bits(text2))
+
+print("\nInciso 2")
+
+bits1 = "01001000011011110110110001100001"
+bits2 = "01000011011010010110011001110010011000010110010001101111"
+
+print("\nEjemplo 1:")
+print("Bits:", bits1)
+print("Texto:", bits_to_text(bits1))
+
+print("\nEjemplo 2:")
+print("Bits:", bits2)
+print("Texto:", bits_to_text(bits2.replace(" ", "")))
+
+print ("\nInciso 3")
+
+text1 = "Hola"
+text2 = "Cifrado"
+
+print("\nEjemplo 1:")
+print("Texto:", text1)
+print("Base64:", text_to_base64(text1))
+
+print("\nEjemplo 2:")
+print("Texto:", text2)
+print("Base64:", text_to_base64(text2))
